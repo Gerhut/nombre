5a58bd00849b048d3d6ccc1db19316d0bab6ef36
George Chung <Gerhut@GMail.com>
2017-06-17 16:33:30 +0800

    Spilt test cases

--- test.js
+++ test.js
@@ -4,6 +4,12 @@ var nombre = require('.')
 
 it('should convert integer in any radix into decimal integer', function () {
   nombre('F', 16).should.equal(15)
+})
+
+it('should convert float in any radix into decimal float', function () {
   nombre('0.1', 2).should.equal(0.5)
+})
+
+it('should convert unknown numbers into NaN', function () {
   nombre('boom', 5).should.be.NaN()
 })
